0bf039e120ec2385f74d9ab893a2258bb2d87866
wndnjs00 <juwonjeon852@gmail.com>
2025-10-02 22:11:47 +0900

    카탈로그 앱 구현(리사이클러뷰처럼)

--- part1-chapter3-18/app/src/main/java/kr/co/fastcampus/part1/chapter3_18/MainActivity.kt
+++ part1-chapter3-18/app/src/main/java/kr/co/fastcampus/part1/chapter3_18/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.part1.chapter3_18.R.drawable
@@ -40,6 +48,24 @@ fun Item(itemData: ItemData) {
         elevation = 8.dp,
         modifier = Modifier.padding(16.dp)
     ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ){
+            Image(
+                painter = painterResource(id = itemData.imageId),
+                contentDescription = "이미지"
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.title,
+                style = MaterialTheme.typography.h4
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(
+                text = itemData.description
+            )
+        }
+
     }
 }
 
@@ -59,9 +85,14 @@ fun ItemPreview() {
 
 @Composable
 fun CatalogEx(itemList: List<ItemData>) {
+    // 리스트뷰, 리사이클러뷰를 대체하기위해 LazyColumn 사용
+    // items는 LazyColumn에 있는 기본속성
     LazyColumn {
         // 스텝 2: `items(itemList)`를 이용해 Item을 반복해서
-        // 컬럼에 추가하세요.
+        // 컬럼에 추가하세요
+        items(itemList){ item ->
+            Item(item)
+        }
     }
 }
 
